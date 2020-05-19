fa379019c5180dcc1a3d7cc8ddf8317dfced150c
Ydot12344 <64800705+Ydot12344@users.noreply.github.com>
2020-05-20 01:10:53 +0300

    Код формы
    Добавил strip menu

--- HW/HW/Form1.Designer.cs
+++ HW/HW/Form1.Designer.cs
@@ -29,15 +29,53 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.таблицаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.графикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.таблицаToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(0, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.Size = new System.Drawing.Size(800, 426);
             this.panel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.таблицаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // таблицаToolStripMenuItem
+            // 
+            this.таблицаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.графикToolStripMenuItem,
+            this.таблицаToolStripMenuItem1});
+            this.таблицаToolStripMenuItem.Name = "таблицаToolStripMenuItem";
+            this.таблицаToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.таблицаToolStripMenuItem.Text = "Вид";
+            // 
+            // графикToolStripMenuItem
+            // 
+            this.графикToolStripMenuItem.Name = "графикToolStripMenuItem";
+            this.графикToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.графикToolStripMenuItem.Text = "График";
+            this.графикToolStripMenuItem.Click += new System.EventHandler(this.графикToolStripMenuItem_Click_1);
+            // 
+            // таблицаToolStripMenuItem1
+            // 
+            this.таблицаToolStripMenuItem1.Name = "таблицаToolStripMenuItem1";
+            this.таблицаToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.таблицаToolStripMenuItem1.Text = "Таблица";
+            this.таблицаToolStripMenuItem1.Click += new System.EventHandler(this.таблицаToolStripMenuItem1_Click);
             // 
             // Form1
             // 
@@ -45,15 +83,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem таблицаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem графикToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem таблицаToolStripMenuItem1;
     }
 }
 
--- HW/HW/Form1.cs
+++ HW/HW/Form1.cs
@@ -5,16 +5,31 @@ namespace HW
 {
     public partial class Form1 : Form
     {
+        private Data data;
         public Form1()
         {
-            Data data = new Data();
+            data = new Data();
             InitializeComponent();
+            panel1.Controls.Clear();
+
+            
+        }
+
+        private void графикToolStripMenuItem_Click_1(object sender, System.EventArgs e)
+        {
+            panel1.Controls.Clear();
+
             Graph g = new Graph(data.input);
 
             g.Dock = DockStyle.Fill;
 
             panel1.Controls.Clear();
             panel1.Controls.Add(g);
+        }
+
+        private void таблицаToolStripMenuItem1_Click(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
